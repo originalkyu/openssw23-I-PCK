7e9288979f2c9c4441af55f84bf8ffb1408af21d
originalkyu <originalkyu@gmail.com>
2023-06-08 07:05:55 +0900

    6.8 07:05

--- basicsr/version.py
+++ basicsr/version.py
@@ -1,5 +1,5 @@
 # GENERATED VERSION FILE
-# TIME: Mon Apr 18 21:35:20 2022
-__version__ = '1.2.0+386ca20'
+# TIME: Thu Jun  8 07:04:19 2023
+__version__ = '1.2.0+9166345'
 short_version = '1.2.0'
 version_info = (1, 2, 0)
